7e52f0d13db28a7ba00de56fd2682bea72b301b8
Carlos Monteiro <carlos_armani4@uri.edu>
2023-12-02 17:28:22 -0500

    added some more functionality to main

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@
 
 // hello
 
+int SerialNumberGenerator(std::string NF_F, std::string genre, std::string type, std::string length){
+
+}
+
+
 int main(int argc, char*argv[]){
    std::string fname(argv[1]);
    BookTree tree;
@@ -16,11 +21,35 @@ int main(int argc, char*argv[]){
     while(std::getline(File, line)){
       
       std::stringstream streamline(line);
-  
-      int num;
+
+      std::string title;
+      std::string author;
+      std::string NF_F;
+      std::string genre;
+      std::string type;
+      std::string length;
+
+      int Serial;
+
+      int i = 0;
       while(std::getline(streamline, entry, ',')){
-          tree.insert(num);
+          if(i == 0){
+              title = entry;
+          }else if(i == 1){
+              author = entry;
+          }else if(i == 2){
+              NF_F = entry;
+          }else if(i == 3){
+              genre = entry;
+          }else if(i == 4){
+              type = entry;
+          }else if(i == 5){
+              length = entry;
+          }
+          i++;
       }
+      Serial = SerialNumberGenerator;
+      tree.insert(Serial, title, author);
     }
     File.close;
     
