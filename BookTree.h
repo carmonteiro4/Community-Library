0f1f81bf255b41678a15a839795598417404929a
jamesdfurtado <jamesdfurtado@uri.edu>
2023-12-02 17:06:58 -0500

    Create necessary functions for BookTree.h

--- BookTree.h
+++ BookTree.h
@@ -3,15 +3,11 @@
 
 #include <iostream>
 #include <ostream>
-//include "book.h"
-
-class BookTree {
-
-};
+#include "book.h"
 
 class BookTreeNode {
     private:
-        //Book book;
+        Book book;
         int serial;
         bool color;
         BookTreeNode* left;
@@ -19,8 +15,32 @@ class BookTreeNode {
 
     public:
         BookTreeNode();
-        BookTreeNode(int node_val);
+        BookTreeNode(Book book);
         friend class BookTree;
+};
+
+class BookTree {
+    private:
+        BookTreeNode* root;
+        BookTreeNode* insert(Book book, BookTreeNode* root);
+
+        void flipColors(BookTreeNode* root);
+        BookTreeNode* rotateLeft(BookTreeNode* root);
+        BookTreeNode* rotateRight(BookTreeNode* root);
+
+        int height(BookTreeNode* root);
+        bool is_red(BookTreeNode* root);
+
+    public:
+        LLRBTree();
+
+        void insert(Book book);
+
+        void preorder(std::ostream& os= std::cout);
+        void postorder(std::ostream& os= std::cout);
+        void inorder(std::ostream& os = std::cout);
+        void search(int serialNum);
+        int height();
 };
 
 #endif //CSC_212_PROJECT_BOOKTREE_H
