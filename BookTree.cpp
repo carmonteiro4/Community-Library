e3941887928d1591f2c0a90f799488c4c45ac4e1
jamesdfurtado <jamesdfurtado@uri.edu>
2023-12-02 16:10:35 -0500

    Creation of BookTree
    Created BookTree and BookTreeNode header and cpp files

---
+++ BookTree.cpp
@@ -0,0 +1,2 @@
+#include "BookTree.h"
+
---
+++ BookTree.h
@@ -0,0 +1,12 @@
+#ifndef CSC_212_PROJECT_BOOKTREE_H
+#define CSC_212_PROJECT_BOOKTREE_H
+
+class BookTree {
+
+};
+
+class BookTreeNode {
+
+};
+
+#endif //CSC_212_PROJECT_BOOKTREE_H
