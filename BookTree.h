8127bbf04d4fb48eeb72b573af70f33b491a36e3
jamesdfurtado <jamesdfurtado@uri.edu>
2023-12-02 16:32:38 -0500

    Set skeleton for BookTreeNode

--- BookTree.h
+++ BookTree.h
@@ -1,12 +1,26 @@
 #ifndef CSC_212_PROJECT_BOOKTREE_H
 #define CSC_212_PROJECT_BOOKTREE_H
 
+#include <iostream>
+#include <ostream>
+//include "book.h"
+
 class BookTree {
 
 };
 
 class BookTreeNode {
+    private:
+        //Book book;
+        int serial;
+        bool color;
+        BookTreeNode* left;
+        BookTreeNode* right;
 
+    public:
+        BookTreeNode();
+        BookTreeNode(int node_val);
+        friend class BookTree;
 };
 
 #endif //CSC_212_PROJECT_BOOKTREE_H
