b56254aef8d8eb3001cd0f45420078654681b0c2
carmonteiro4 <123407423+carmonteiro4@users.noreply.github.com>
2023-12-02 16:19:18 -0500

    Update main.cpp
    main can take in file

--- main.cpp
+++ main.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
 
+int main(int argc, char*argv[]){
+   std::string fname(argv[1]);
+   BookTree tree;
+
+    std::string line;
+    std::string entry;
+    std::ifstream File;
+
+    std::getline(File, line);
+    while(std::getline(File, line)){
+      
+      std::stringstream streamline(line);
+  
+      int num;
+      while(std::getline(streamline, entry, ',')){
+          tree.insert(num);
+      }
+    }
+    File.close;
+    
+return 0;
+}
